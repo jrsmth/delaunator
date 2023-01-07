f7b1e7a620f4110bdfb936c403f0798c0db3883d
JRSmiffy <jamesrichardsmith97@gmail.com>
2023-01-07 21:31:15 +0000

    TS Lint updates

--- src/delaunay.ts
+++ src/delaunay.ts
@@ -3,23 +3,23 @@ import { Point } from './shape/point';
 export class Delaunay {
 
   public static generatePoints(width: number, height: number, numPoints: number): Point[] {
-    let pointSet: Point[] = [];
-    let xList: number[] = [];
-    let yList: number[] = [];
+    const pointSet: Point[] = [];
+    const xList: number[] = [];
+    const yList: number[] = [];
 
     for (let i = 1; i <= numPoints; i++) {
       let newPointRequired = true;
       while (newPointRequired) {
-        let candidate = this.generateRandomPoint(width, height);
+        const candidate = this.generateRandomPoint(width, height);
 
-        let uniqueCandidate = !((xList.includes(candidate.x)) || (yList.includes(candidate.y)));
+        const uniqueCandidate = !((xList.includes(candidate.x)) || (yList.includes(candidate.y)));
         if (uniqueCandidate) {
           pointSet.push(candidate);
           xList.push(candidate.x);
           yList.push(candidate.y);
 
           newPointRequired = false;
-        } else console.debug(`Duplicate candidate found! (x: ${candidate.x}, y: ${candidate.y})`);
+        } // else console.debug(`Duplicate candidate found! (x: ${candidate.x}, y: ${candidate.y})`);
       }
     }
 
@@ -27,15 +27,15 @@ export class Delaunay {
   }
 
   private static generateRandomPoint(width: number, height: number){
-    let borderRatio = 0.1;
-    let xMax = width * (1 - borderRatio);
-    let yMax = height * (1 - borderRatio);
+    const borderRatio = 0.1;
+    const xMax = width * (1 - borderRatio);
+    const yMax = height * (1 - borderRatio);
 
-    let xMin = width * borderRatio;
-    let yMin = height * borderRatio;
+    const xMin = width * borderRatio;
+    const yMin = height * borderRatio;
 
-    let xCoord = Math.floor((xMin + Math.random() * xMax)) - xMin;
-    let yCoord = Math.floor((yMin + Math.random() * yMax)) - yMin;
+    const xCoord = Math.floor((xMin + Math.random() * xMax)) - xMin;
+    const yCoord = Math.floor((yMin + Math.random() * yMax)) - yMin;
 
     return new Point(xCoord, yCoord);
   }
--- src/test/delaunay.test.ts
+++ src/test/delaunay.test.ts
@@ -7,28 +7,28 @@ describe("Delaunay Tests", () => {
     // Note: Unique refers to distinctness across all values of x & y, rather than just distinct pairs of (x,y)
 
     // Given
-    let width = 1000; let height = 1000; let numPoints = 100;
+    const width = 1000; const height = 1000; const numPoints = 100;
 
     // When
-    let points: Point[] = Delaunay.generatePoints(width, height, numPoints);
+    const points: Point[] = Delaunay.generatePoints(width, height, numPoints);
 
     // Then
     expect(points.length).toBe(numPoints);
 
-    let xSet = points.map(pt => pt.x);
-    let ySet = points.map(pt => pt.y);
+    const xSet = points.map(pt => pt.x);
+    const ySet = points.map(pt => pt.y);
 
     // all x's and y's are unique
     expect(xSet.length).toEqual(new Set(xSet).size)
     expect(ySet.length).toEqual(new Set(ySet).size)
 
     // all x's/y's are between zero and width/height
-    for (let ordinate of xSet) {
+    for (const ordinate of xSet) {
       expect(ordinate).toBeGreaterThan(0);
       expect(ordinate).toBeLessThan(width);
     }
 
-    for (let ordinate of ySet) {
+    for (const ordinate of ySet) {
       expect(ordinate).toBeGreaterThan(0);
       expect(ordinate).toBeLessThan(width);
     }
